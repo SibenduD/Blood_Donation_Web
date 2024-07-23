1c5af60e45f9091629df1ddc569df2a27d3db2ba
SibenduD <sibendudas.official@gmail.com>
2024-07-23 20:51:37 +0530

    adding directly the whole project after  completion

---
+++ include/footer.php
@@ -0,0 +1,34 @@
+<!-- contact us section -->
+			<div class="container-fluid">					
+				<div class="row contactus">
+					<div class="col-md-6 offset-md-3">
+						<h1 class="text-center">Contact Us</h1>
+						<hr class="white-bar">
+
+							<h1 class="text-center">
+								<a  target="_blank" href="https://www.facebook.com/profile.php?id=100088231096268"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
+
+								<a target="_blank" href="https://www.twitter.com/wires_for_veins"><i style="color: #fff;" class="fab fa-twitter-square"></i></a> 
+
+								<a target="_blank" href="mailto:wiresforveins06@gmail.com"><i style="color: #fff;" class="fab fa fa-envelope"></i></a> 
+							    
+							</h1>
+
+							<p class="text-center">@copyright 2023 wiresforveins</p>
+
+					</div>
+				</div>
+			</div>
+			<!-- end contact us section -->
+			
+	    <!-- JQuery File -->
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    
+    <!-- BootStrap JS File-->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Fontawesome Icon JS-->
+    <script defer src="fontawesome/js/all.js"></script>
+
+	</body>
+</html>
